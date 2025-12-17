7c580483ebe81d825727c6407963fb974c21e5b0
Elvin Agazada <elvinagazad086a@gmail.com>
2025-12-17 16:50:45 +0400

    The names of endpoints were changed

--- src/main/java/az/kapitalbank/mb/bff/transfermobile/controllers/CustomerController.java
+++ src/main/java/az/kapitalbank/mb/bff/transfermobile/controllers/CustomerController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/v1/transfers/customer")
+@RequestMapping("/api/v1/transfer")
 @RequiredArgsConstructor
 public class CustomerController {
 
