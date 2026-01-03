c8a981f4ee8b3f5abc12a40e2ba4e14d37e6a3e5
Elvin Agazada <elvinagazad086a@gmail.com>
2026-01-03 19:35:53 +0400

    In account service exception classes were fixed

--- src/main/java/az/kapitalbank/mb/bff/transfermobile/exceptions/AccountAlreadyExistsException.java
+++ src/main/java/az/kapitalbank/mb/bff/transfermobile/exceptions/AccountAlreadyExistsException.java
@@ -1,6 +1,12 @@
 package az.kapitalbank.mb.bff.transfermobile.exceptions;
 
+import java.io.Serial;
+
 public class AccountAlreadyExistsException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     public AccountAlreadyExistsException(Long id) {
         super("Account already exists for customer id: " + id);
     }
--- src/main/java/az/kapitalbank/mb/bff/transfermobile/exceptions/InvalidAmountException.java
+++ src/main/java/az/kapitalbank/mb/bff/transfermobile/exceptions/InvalidAmountException.java
@@ -1,6 +1,12 @@
 package az.kapitalbank.mb.bff.transfermobile.exceptions;
 
+import java.io.Serial;
+
 public class InvalidAmountException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     public InvalidAmountException() {
         super("Amount must be greater than zero");
     }
